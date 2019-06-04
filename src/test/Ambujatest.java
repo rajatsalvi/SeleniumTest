a040924fc386cdaa6568d04807ea70109bcff878
rajatsalvi <rajatsalvi6@gmail.com>
2019-06-04 18:41:01 +0530

    Changes are made

--- src/test/Ambujatest.java
+++ src/test/Ambujatest.java
@@ -2,6 +2,7 @@ package test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Ambujatest {
@@ -26,15 +27,35 @@ public class Ambujatest {
 		
 		driver.findElement(By.xpath("/html/body/my-app/ng-component/div/div[2]/div[2]/div/form/ul/li[3]/button[1]")).click();
 		
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		
-		driver.findElement(By.xpath("/html/body/my-app/menubar/div[1]/div/div[4]/div/a/span/i")).click();
-		
-		Thread.sleep(2000);
-		
-		driver.findElement(By.xpath("/html/body/my-app/menubar/div[1]/div/div[4]/div/ul/li[3]/a")).click();
-		
-		
+	
+			if(driver.findElement(By.xpath("/html/body/my-app/menubar/p-dialog[3]/div")).isDisplayed() )                                                                                                         
+			{         
+				Thread.sleep(3000);
+			  /**Do this*/     
+				driver.findElement(By.xpath("/html/body/my-app/menubar/p-dialog[3]/div/div[2]/div/button[2]")).click();
+				
+				Thread.sleep(2000);
+				
+				
+				driver.findElement(By.xpath("/html/body/my-app/menubar/div[1]/div/div[4]/div/a/span/i")).click();
+				
+				Thread.sleep(2000);
+				
+				driver.findElement(By.xpath("/html/body/my-app/menubar/div[1]/div/div[4]/div/ul/li[3]/a")).click();
+				
+			}    
+			else    
+			{     
+			  /**Do this*/   
+				driver.findElement(By.xpath("/html/body/my-app/menubar/div[1]/div/div[4]/div/a/span/i")).click();
+				
+				Thread.sleep(2000);
+				
+				driver.findElement(By.xpath("/html/body/my-app/menubar/div[1]/div/div[4]/div/ul/li[3]/a")).click();	
+				
+			}
 		
 		
 		
